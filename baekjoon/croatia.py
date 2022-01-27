0260753c10c319464e1a0e4b7d6b0ec250d13383
PolarbearHam <gkagu12@naver.com>
2022-01-27 20:15:27 +0900

    day5

--- baekjoon/croatia.py
+++ baekjoon/croatia.py
@@ -2,11 +2,6 @@ arr = ['c=', 'c-', 'dz=', 'd-',
        'lj','nj', 's=', 'z=']
 a = input()
 c = 0
-stack = ''
-for i in a:
-       stack += i
-       if stack in arr:
-              stack
-       else:
-              c+=1
-print(c)
+for i in arr:
+       a = a.replace(i, 'a')
+print(len(a))
